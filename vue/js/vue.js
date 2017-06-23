7a13a673df96a9f335eef5b6283fb20f7e666bd4
wilhelmina <wilhelminaola05@gmail.com>
2017-06-23 16:41:01 -0700

    Add src

--- vue/js/vue.js
+++ vue/js/vue.js
@@ -2,12 +2,13 @@
  * Created by Windows 10 Pro on 6/23/2017.
  */
 // https://jsfiddle.net/jpeter06/ppyeo1tg/
+//https://stackoverflow.com/questions/43250649/vue-component-and-alert-on-click-button
 
 Vue.config.devtools = true;
 
 Vue.component('todo-list',{
     props:['atodo'],
-    template: '<li @dbClick><button type="button" @click.prevent="del($index)">X</button>&nbsp;&nbsp; {{atodo.text}} </li>'
+    template: '<li @dbClick><button type="button" v-on:click="del">X</button>&nbsp;&nbsp; {{atodo.text}} </li>'
 })
 
  var oTodo = new Vue({
